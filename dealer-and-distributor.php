8b6a1273c4cfb0aa97204865031ee90df4c14db3
gurumoorthy <gurumoorthy.s@zencode.guru>
2020-08-21 19:08:06 +0530

    dealer pages

---
+++ dealer-and-distributor.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin 2 - Dashboard</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <div id="wrapper">
+
+   
+   
+   <?php
+include 'sidebar.php';
+?>
+
+ 
+    <div id="content-wrapper" class="d-flex flex-column">
+
+
+      <div id="content">
+
+    
+      <?php include 'header.php'; ?>
+        
+      <div class="container-fluid">
+          <div class="row">
+            <form class="company-creation-form">
+                <div class="form-group">
+                  <label for="exampleInputEmail1"> Name</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter your Name">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Mobile</label>
+                  <input type="number" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Mobile number">
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Email</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlTextarea1">Address</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Company Name</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Company Name">
+                 
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">RTO</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="" Readonly>
+                 
+                </div>
+                <div class="form-group choose-image">
+                    <label for="exampleFormControlFile1">Choose Logo</label>
+                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                  </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Select Dealer are Distributor</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                    <option>Dealer</option>
+                    <option>Distributor</option>
+                   
+                    </select>
+                </div>
+            
+                
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </form>
+          </div>
+      </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      
+  <?php include 'footer.php' ?>
+     
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+ 
+
+  <!-- Logout Modal-->
+  <!-- <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+
+
+</body>
+
+</html>
