afed9e853fe95420156d5054a9defe4b369920d2
gurumoorthy <gurumoorthy.s@zencode.guru>
2020-08-22 17:17:10 +0530

    customer pages completed

---
+++ customer.php
@@ -0,0 +1,233 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin 2 - Dashboard</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <div id="wrapper">
+
+   
+   
+   <?php
+include 'sidebar.php';
+?>
+
+ 
+    <div id="content-wrapper" class="d-flex flex-column">
+
+
+      <div id="content">
+
+    
+      <?php include 'header.php'; ?>
+        
+      <div class="container-fluid">
+      <h5 class="text-center">New Entry </h5> 
+      <div class="stock">
+      <p>Available Stock </p>
+      <ul>
+      <li>Red 20mm:<span>0.00 Mtrs</span> </li>
+      <li>Red 20mm:<span>0.00 Mtrs</span> </li>
+      <li>Red 20mm:<span>0.00 Mtrs</span> </li>
+      <li>Red 20mm:<span>0.00 Mtrs</span> </li>
+      <li>Red 20mm:<span>0.00 Mtrs</span> </li>
+      <li>Red 20mm:<span>0.00 Mtrs</span> </li>
+      <li>Red 20mm:<span>0.00 Mtrs</span> </li>
+      </div>
+          <div class="row">
+         
+            <form class="company-creation-form">
+            <div class="form-group">
+                  <label for="exampleInputEmail1">Date</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="01-01-2021" readonly>
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1"> Vehicle RegNo:</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Vehicle Number">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1"> Vehicle Manufacturing Year:</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Classis No:</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Vehicle Number">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1"> Engine No:</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Vehicle Number">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1"> Vechicle Make:</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Vehicle Number">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Vechicle Model:</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Vehicle Number">
+                </div>
+              
+                <div class="form-group">
+                  <label for="exampleInputEmail1"> Owner Name</label>
+                  <input type="text" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Name">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Phone</label>
+                  <input type="number" class="form-control" id="exampleCompanyName1" aria-describedby="emailHelp" placeholder="Enter Mobile number">
+                </div>
+                <div class="form-group">
+                  <label for="exampleFormControlTextarea1">Address</label>
+                  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">RTO</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="" readonly>
+                </div>
+                
+            <div class="border-bottom mb-3">
+               <h6 class="text-center"> Reflective Tape details </h6>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">Red 20mm:</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">white 20mm:</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+            </div><div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">Red 50mm:</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">white 50mm:</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">yellow 50mm:</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">80mm red circular reflector:</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">80mm white circular reflector:</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">80mm yellow circular reflector:</label>
+              <div class="col-sm-8">
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">Class 3:</label>
+              <div class="col-sm-8">
+                <input type="" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label for="" class="col-sm-4 col-form-label">Class 4:</label>
+              <div class="col-sm-8">
+                <input type="" class="form-control" id="" placeholder="">
+              </div>
+            </div>
+            <div class="border-bottom mb-3">
+               <h6 class="text-center"> Images</h6>
+            </div>
+            <div class="form-group choose-image">
+                    <label for="exampleFormControlFile1">Front Image</label>
+                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+            <div class="form-group choose-image">
+                    <label for="exampleFormControlFile1">Left Image</label>
+                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+            <div class="form-group choose-image">
+                    <label for="exampleFormControlFile1">Right  Image</label>
+                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>
+            <div class="form-group choose-image">
+                    <label for="exampleFormControlFile1">Back Image</label>
+                    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+            </div>    
+            <div class="text-center">
+            <button type="submit" class="btn btn-primary w-50">Save</button>
+            </div>
+            
+              
+              </form>
+          </div>
+      </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      
+  <?php include 'footer.php' ?>
+     
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+ 
+
+  <!-- Logout Modal-->
+  <!-- <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="login.html">Logout</a>
+        </div>
+      </div>
+    </div>
+  </div> -->
+
+
+
+</body>
+
+</html>
